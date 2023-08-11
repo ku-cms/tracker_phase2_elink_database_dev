0b00348be9efd2fcb582edd7534cd81f8f038fd2
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-08-11 16:07:51 +0200

    Add user_name to users table.

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
+    user_name VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     favorite_color VARCHAR(255)
 );
